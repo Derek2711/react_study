fe7583a55af2c44e4ac7e04f3408308beba0341f
derek <szhtike004@gmail.com>
2023-12-07 19:41:41 +0630

    [add] header with full responsive

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,31 @@
-export function button(){
-    return <button />
+import { VariantProps, cva } from "class-variance-authority"
+import { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
+
+const buttonStyles = cva(["transition-colors"],
+    {
+        variants: {
+            variant: {
+                default: ["hover:bg-secondary-hover", "bg-secondary"],
+                ghost: ["hover:bg-gray-100"]
+            },
+            size: {
+                default: ["rounded", "p-2"],
+                icon: ["rounded-full", "w-10", "h-10", "flex", "items-center", "justify-center", "p-2.5"]
+            }
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default"
+        }
+    }
+)
+
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">
+
+export function Button({ variant, size, className, ...props }: ButtonProps) {
+    return (
+        <button
+            {...props} className={twMerge(buttonStyles({ variant, size }), className)}
+         />)
 }
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,19 +1,57 @@
 import LogoYoutube from "../assets/LogoYoutube";
-import { Menu } from "lucide-react";
+import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
+import { Button } from "../components/Button";
+import { useState } from "react";
 
 export default function PageHeader() {
+    const [showFullWidthSearch, setshowFullWidthSearch] = useState(false);
+
     return (
-        <div className="flex justify-between gap-10 lg:gap-20">
-            <div className="flex flex-shrink-0 gap-4 items-center">
-                <button>
+        <div className="flex justify-between gap-10 lg:gap-20 pt-2 mb-6 mx-4 mt-1">
+            <div className={`flex-shrink-0 gap-4 items-center ${showFullWidthSearch ? "hidden" : "flex"}`}>
+                <Button variant="ghost" size="icon">
                     <Menu />
-                </button>
+                </Button>
                 <a href="/">
                     <LogoYoutube />
                 </a>
             </div>
-            <div></div>
-            <div></div>
+            <form className={`gap-4 justify-center flex-grow ${showFullWidthSearch ? "flex" : "md:flex hidden"}`}>
+                {showFullWidthSearch && (
+                    <Button type="button" variant="ghost" size="icon" onClick={() => setshowFullWidthSearch(false)}>
+                        <ArrowLeft />
+                    </Button>
+                )}
+                <div className="flex flex-grow max-w-[600px]">
+                    <input type="search" placeholder="Search"
+                        className="rounded-l-full px-5 border border-secondary-border
+                    shadow-inner shadow-secondary w-full outline-none focus:border-blue-500"/>
+                    <Button className="py-2 px-4 rounded-r-full border border-secondary-border
+                     border-l-0 flex-shrink-0">
+                        <Search />
+                    </Button>
+                </div>
+                <Button type="button" size="icon" className="flex-shrink-0">
+                    <Mic />
+                </Button>
+            </form>
+            <div className={`flex-shrink-0 md:gap-2 ${showFullWidthSearch ? "hidden" : "flex"}`}>
+                <Button onClick={() => setshowFullWidthSearch(true)} variant="ghost" size="icon" className="md:hidden">
+                    <Search />
+                </Button>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Mic />
+                </Button>
+                <Button variant="ghost" size="icon">
+                    <Upload />
+                </Button>
+                <Button variant="ghost" size="icon">
+                    <Bell />
+                </Button>
+                <Button variant="ghost" size="icon">
+                    <User />
+                </Button>
+            </div>
         </div>
     )
 }
